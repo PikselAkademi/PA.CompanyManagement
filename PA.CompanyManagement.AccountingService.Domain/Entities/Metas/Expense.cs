020a65635756f75b556c0732c61328886013f9b9
Kadir Berat GÜVENTÜRK Piksel Akademi (Antalya Şb) <kadir@pikselakademi.com>
2025-12-27 16:47:30 +0300

    Accounting.Applicaiton -DTO Requests Created.

--- PA.CompanyManagement.AccountingService.Domain/Entities/Metas/Expense.cs
+++ PA.CompanyManagement.AccountingService.Domain/Entities/Metas/Expense.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PA.CompanyManagement.Core.Domain.Entities.Base;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Text;
 namespace PA.CompanyManagement.AccountingService.Domain.Entities.Metas
 {
     [Table("Expenses", Schema = "meta")]
-    public class Expense
+    public class Expense : BaseEntity
     {
         public string? Title { get; set; }
         public string? Description { get; set; }
