16294d5779fb9de4daf211cc677dea07651494a1
Kadir Berat GÜVENTÜRK Piksel Akademi (Antalya Şb) <kadir@pikselakademi.com>
2025-12-27 17:30:27 +0300

    Accounting.Application Responses & Meta Repositories Created.

--- PA.CompanyManagement.AccountingService.Application/Repositories/Metas/IIncomeRepository.cs
+++ PA.CompanyManagement.AccountingService.Application/Repositories/Metas/IIncomeRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PA.CompanyManagement.AccountingService.Application.DTOs.Requests.Metas;
+using PA.CompanyManagement.AccountingService.Application.DTOs.Responses.Metas;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,17 @@ namespace PA.CompanyManagement.AccountingService.Application.Repositories.Metas
 {
     public interface IIncomeRepository
     {
+        Task<List<MinimalIncomeResponse>> GetAllAsync();
+        Task<List<MinimalIncomeResponse>> GetAllAsync(Guid expenseTypeId);
+
+        Task<IncomeResponse> GetAsync(Guid id);
+        Task<DetailedIncomeResponse> GetDetailedAsync(Guid id);
+
+        Task<IncomeResponse> CreateAsync(IncomeCreateRequest request);
+
+        Task UpdateAsync(IncomeUpdateRequest request);
+        Task PatchAsync(IncomePatchRequest request);
+
+        Task DeleteAsync(Guid id);
     }
 }
