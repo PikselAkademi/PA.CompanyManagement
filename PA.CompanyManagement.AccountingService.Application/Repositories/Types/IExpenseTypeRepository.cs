61bc11068f29ad72f164be81d44a96464a6da547
Kadir Berat GÜVENTÜRK Piksel Akademi (Antalya Şb) <kadir@pikselakademi.com>
2026-01-03 15:44:33 +0300

    AccountingService -Applications Repositories Completed -Infrastructure Context Created

--- PA.CompanyManagement.AccountingService.Application/Repositories/Types/IExpenseTypeRepository.cs
+++ PA.CompanyManagement.AccountingService.Application/Repositories/Types/IExpenseTypeRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PA.CompanyManagement.AccountingService.Application.DTOs.Requests.Metas;
+using PA.CompanyManagement.AccountingService.Application.DTOs.Requests.Types;
+using PA.CompanyManagement.AccountingService.Application.DTOs.Responses.Types;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +9,16 @@ namespace PA.CompanyManagement.AccountingService.Application.Repositories.Types
 {
     public interface IExpenseTypeRepository
     {
+        Task<List<ExpenseTypeResponse>> GetAllAsync();
+
+        Task<ExpenseTypeResponse> GetAsync(Guid id);
+        Task<DetailedExpeseTypeResponse> GetDetailedAsync(Guid id);
+
+        Task<ExpenseTypeResponse> CreateAsync(ExpenseTypeCreateRequest request);
+
+        Task UpdateAsync(ExpenseTypeUpdateRequest request);
+
+        Task DeleteAsync(Guid id);
+
     }
 }
---
+++ PA.CompanyManagement.AccountingService.Infrastructure/Contexts/AccountingDBContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PA.CompanyManagement.AccountingService.Domain.Entities.Metas;
+using PA.CompanyManagement.AccountingService.Domain.Entities.Types;
+using PA.CompanyManagement.Core.Domain.Settings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PA.CompanyManagement.AccountingService.Infrastructure.Contexts
+{
+    public class AccountingDBContext : DbContext
+    {
+        private readonly IConfiguration? _conf;
+        private readonly ICurrentUser? _currentUser;
+
+        public AccountingDBContext() { }
+        public AccountingDBContext(DbContextOptions<AccountingDBContext> options) : base(options) { }
+
+        public AccountingDBContext(IConfiguration conf) => _conf = conf;
+        public AccountingDBContext(DbContextOptions<AccountingDBContext> options, IConfiguration conf)
+            : base(options) => _conf = conf;
+
+        public AccountingDBContext(ICurrentUser currentUser) => _currentUser = currentUser;
+        public AccountingDBContext(DbContextOptions<AccountingDBContext> options, ICurrentUser currentUser)
+            : base(options) => _currentUser = currentUser;
+
+        public AccountingDBContext(IConfiguration conf, ICurrentUser currentUser)
+        {
+            _conf = conf;
+            _currentUser = currentUser;
+        }
+        public AccountingDBContext(DbContextOptions<AccountingDBContext> options,
+            IConfiguration conf,
+            ICurrentUser currentUser) : base(options)
+        {
+            _conf = conf;
+            _currentUser = currentUser;
+        }
+
+        public DbSet<IncomeType> IncomeTypes { get; set; }
+        public DbSet<ExpenseType> ExpenseTypes { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
