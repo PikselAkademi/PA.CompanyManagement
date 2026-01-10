ac220bf1e68c0e5a2f9a490dc45fd56bb39dc711
Kadir Berat GÜVENTÜRK Piksel Akademi (Antalya Şb) <kadir@pikselakademi.com>
2026-01-10 15:43:53 +0300

    update

---
+++ PA.CompanyManagement.Core/Exceptions/PAContextUncatchedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PA.CompanyManagement.Core.Exceptions
+{
+    public class PAContextUncatchedException : Exception
+    {
+        public PAContextUncatchedException() { }
+        public PAContextUncatchedException(string message) : base(message) { }
+        public PAContextUncatchedException(string message, Exception? innerException) : base(message, innerException) { }
+
+    }
+}
